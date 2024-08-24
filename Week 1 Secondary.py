3cca1271a17423687d87cbb9b9df829f49b500f0
Davey Butler <dbutler2@hawaii.edu>
2024-08-23 22:41:27 -1000

    First Commit

---
+++ Week 1 Secondary.py
@@ -0,0 +1,23 @@
+#Pick one of the three following challenges to complete, and put the solving code in this file.
+# The codewars link is included if you would like to use their 
+# testing system to verify your answer, and read discussions and see 
+# other solutions after you completed it.
+# When using codewars or doing these solutions make sure to use a function and return a value. Contact me if you are unsure what I mean.
+# Feel free to do all three if you would like.
+
+
+#Number 1 source: https://www.codewars.com/kata/546e2562b03326a88e000020/python
+#Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+#For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+#Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+#Note: The function accepts an integer and returns an integer.
+#Happy Coding!
+
+#Number 2 source: https://www.codewars.com/kata/554e4a2f232cdd87d9000038/train/python
+#Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+#If you want to know more: http://en.wikipedia.org/wiki/DNA
+#In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+#More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+#Number 3 source: https://www.codewars.com/kata/57aa3927e298a757820000a8
+#Description is too long to post here, please check it out on the website.
